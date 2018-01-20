a4135bb399cf3dc46b641548c9126acb2c2506ae
Lazersmoke <lazersmoke@gmail.com>
2018-01-20 14:22:28 -0600

    AltStorage no longer extends ManagedDatasource

--- src/main/java/com/civclassic/altmanager/AltStorage.java
+++ src/main/java/com/civclassic/altmanager/AltStorage.java
@@ -10,29 +10,31 @@ import java.util.UUID;
 
 import vg.civcraft.mc.civmodcore.dao.ManagedDatasource;
 
-public class AltStorage extends ManagedDatasource {
+public class AltStorage {
 	
 	private static final String GET_GROUP = "select groupid from alts where player=?;";
 	private static final String GET_ALTS = "select player from alts where groupid=?;";
 	private static final String ADD_UNASSOC_PLAYER = "insert into alts (groupid,player) select coalesce(max(groupid) + 1,0),? from alts;";
 	private static final String ADD_ASSOC = "replace into alts (groupid,player) values (?,?);";
 
+	private final ManagedDatasource db;
+
 	public AltStorage(AltManager plugin, String user, String pass, String host, int port, String database,
 			int poolSize, long connectionTimeout, long idleTimeout, long maxLifetime) {
-		super(plugin, user, pass, host, port, database, poolSize, connectionTimeout, idleTimeout, maxLifetime);
+		db = new ManagedDatasource(plugin, user, pass, host, port, database, poolSize, connectionTimeout, idleTimeout, maxLifetime);
 		registerMigrations();
-		updateDatabase();
+		db.updateDatabase();
 	}
 
 	public void registerMigrations() {
-		registerMigration(1, true, 
+		db.registerMigration(1, true, 
 				"create table if not exists alts ("
 				+ "groupid bigint not null,"
 				+ "player varchar(40) primary key);");
 	}
 
 	public Integer getAssociationGroup(UUID player) {
-		try (Connection conn = getConnection();
+		try (Connection conn = db.getConnection();
 				PreparedStatement ps = conn.prepareStatement(GET_GROUP)) {
 			ps.setString(1, player.toString());
 			ResultSet res = ps.executeQuery();
@@ -47,7 +49,7 @@ public class AltStorage extends ManagedDatasource {
 
 	public Set<UUID> getAltsByAssociationGroup(int grp) {
 		Set<UUID> alts = new HashSet<UUID>();
-		try (Connection conn = getConnection();
+		try (Connection conn = db.getConnection();
 				PreparedStatement ps = conn.prepareStatement(GET_ALTS)) {
 			ps.setInt(1, grp);
 			ResultSet res = ps.executeQuery();
@@ -61,7 +63,7 @@ public class AltStorage extends ManagedDatasource {
 	}
 
 	public void addUnassociatedPlayer(UUID player) {
-		try (Connection conn = getConnection();
+		try (Connection conn = db.getConnection();
 				PreparedStatement ps = conn.prepareStatement(ADD_UNASSOC_PLAYER)) {
 			ps.setString(1, player.toString());
 			ps.execute();
@@ -71,7 +73,7 @@ public class AltStorage extends ManagedDatasource {
 	}
 
 	public void addAssociations(int groupId, Set<UUID> assocs) {
-		try (Connection conn = getConnection();
+		try (Connection conn = db.getConnection();
 				PreparedStatement ps = conn.prepareStatement(ADD_ASSOC)) {
 			ps.setInt(1, groupId);
 			for(UUID u : assocs) {
